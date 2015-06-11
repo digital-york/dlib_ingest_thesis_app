6fbdf309847417deb82bc9f4a38360acf0e67e86
yf508 <y.frank.feng@gmail.com>
2015-06-11 11:45:00 +0100

    added support for .zip files

--- app/models/uploaded_file.rb
+++ app/models/uploaded_file.rb
@@ -7,7 +7,7 @@ class UploadedFile < ActiveRecord::Base
   validates :uf, presence: true
   validates_size_of :uf, maximum: 500.megabytes,
                     message: "should be no more than 500 MB", if: :uf_changed?
-  validates_property :format, of: :uf, in: [:jpeg, :jpg, :png, :bmp, :pdf, :doc, :docx], case_sensitive: false,
-                    message: "should be either .jpeg, .jpg, .png, .bmp, .pdf, .doc, .docx", if: :uf_changed?
+  validates_property :format, of: :uf, in: [:jpeg, :jpg, :png, :bmp, :pdf, :doc, :docx, :zip], case_sensitive: false,
+                    message: "should be either .jpeg, .jpg, .png, .bmp, .pdf, .doc, .docx, .zip", if: :uf_changed?
 
 end
